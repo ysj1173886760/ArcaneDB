b79eb2f5d0be9239592c46bc2475e85aa77d8a03
ysj1173886760 <1173886760@qq.com>
2022-12-10 16:10:03 +0800

    introduce timer and add bthread worker concurrnecy test

---
+++ src/util/time.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <chrono>
+#include <sys/time.h>
+#include <stdlib.h>
+
+namespace arcanedb {
+namespace util {
+
+constexpr int64_t MicroSec = 1ll;
+constexpr int64_t MillSec = 1000ll * MicroSec;
+constexpr int64_t Second = 1000ll * MillSec;
+
+class Timer {
+public:
+    Timer() {
+        gettimeofday(&start_time_, NULL);
+    }
+
+    /**
+     * @brief Get time elapsed.
+     * unit: microsecond.
+     * @return int64_t 
+     */
+    int64_t GetElapsed() const noexcept {
+        struct timeval end_time;
+        gettimeofday(&end_time, NULL);
+        return (end_time.tv_sec - start_time_.tv_sec) * 1000 * 1000
+            + (end_time.tv_usec - start_time_.tv_usec);
+    }
+
+    /**
+     * @brief 
+     * Reset the timer
+     */
+    void Reset() {
+        gettimeofday(&start_time_, NULL);
+    }
+
+private:
+    struct timeval start_time_;
+};
+
+}
+}
--- test/common/bthread_test.cpp
+++ test/common/bthread_test.cpp
@@ -1,20 +1,41 @@
+#include <chrono>
 #include <gtest/gtest.h>
+#include <thread>
 #include "bthread/bthread.h"
 #include "util/bthread_util.h"
+#include "util/time.h"
+
+namespace arcanedb {
+namespace util {
 
 TEST(BthreadTest, BasicTest) {
     bthread_usleep(100);
     ASSERT_TRUE(true);
 }
 
-void TestConcurrency(int thread_num) {
+bool TestConcurrency(int thread_num) {
+    using namespace std::chrono_literals;
+    std::vector<std::shared_ptr<BthreadFuture<void>>> futures(thread_num);
+    Timer timer;
+    for (int i = 0; i < thread_num; i++) {
+        futures[i] = LaunchAsync([]() {
+            std::this_thread::sleep_for(1s);
+        });
+    }
+    for (int i = 0; i < thread_num; i++) {
+        futures[i]->Wait();
+    }
+    return timer.GetElapsed() < 1.2 * Second;
 }
 
 TEST(BthreadTest, MaximumConcurrencyTest) {
-    int lb = 0;
-    int ub = 16;
-    // while (ub - lb > 1) {
-    //     int mid = (ub + lb) / 2;
+    // skip this test since it's only used for testing maximum concurrency
+    GTEST_SKIP();
+    auto concurrency = bthread_getconcurrency();
+    EXPECT_TRUE(TestConcurrency(concurrency));
+    bthread_setconcurrency(concurrency * 2);
+    EXPECT_TRUE(TestConcurrency(concurrency * 2));
+}
 
-    // }
+}
 }
