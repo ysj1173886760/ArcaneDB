546c82e75e05cf65e1280824a0fdfdfb80e996e9
ysj1173886760 <1173886760@qq.com>
2022-12-10 19:43:08 +0800

    implement wait group

---
+++ test/common/wait_group_test.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file wait_group_test.cpp
+ * @author sheep (ysj1173886760@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#pragma once
+
+#include <atomic>
+#include <gtest/gtest.h>
+#include "util/bthread_util.h"
+#include "util/wait_group.h"
+
+namespace arcanedb {
+namespace util {
+
+TEST(WaitGroupTest, BasicTest) {
+    const int worker_cnt = 10;
+    WaitGroup wg(worker_cnt);
+    std::atomic_int cnt{0};
+    for (int i = 0; i < worker_cnt; i++) {
+        LaunchAsync([&]() {
+            cnt.fetch_add(1, std::memory_order_relaxed);
+            wg.Done();
+        });
+    }
+    wg.Wait();
+    EXPECT_EQ(cnt.load(std::memory_order_relaxed), worker_cnt);
+}
+
+}
+}
