a5b5c038619e1e56c0fc24d86f3b7c4cf148ade2
ysj1173886760 <1173886760@qq.com>
2022-12-11 15:52:55 +0800

    introduce concurrent queue

---
+++ src/util/thread_pool.h
@@ -0,0 +1,10 @@
+/**
+ * @file thread_pool.h
+ * @author sheep (ysj1173886760@gmail.com)
+ * @brief simple thread pool
+ * @version 0.1
+ * @date 2022-12-11
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
---
+++ test/util/concurrent_queue_test.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file concurrent_queue_test.cpp
+ * @author sheep (ysj1173886760@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-12-11
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "util/concurrent_queue/blockingconcurrentqueue.h"
+#include "util/concurrent_queue/concurrentqueue.h"
+#include <gtest/gtest.h>
+
+namespace arcanedb {
+
+TEST(ConcurrentQueueTest, BasicTest) {
+  moodycamel::BlockingConcurrentQueue<int> queue;
+  queue.enqueue(10);
+  int item;
+  queue.wait_dequeue(item);
+  EXPECT_EQ(item, 10);
+}
+
+} // namespace arcanedb
