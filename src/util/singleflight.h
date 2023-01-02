5bf0fca56881d505df365a5cf1486ae3195a9f30
ysj1173886760 <1173886760@qq.com>
2023-01-02 14:24:48 +0800

    introduce single flight

---
+++ src/util/singleflight.h
@@ -0,0 +1,88 @@
+/**
+ * @file singleflight.h
+ * @author sheep (ysj1173886760@gmail.com)
+ * @brief port from singleflight from go
+ * @version 0.1
+ * @date 2023-01-02
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#pragma once
+
+#include <string>
+
+#include "absl/container/flat_hash_map.h"
+#include "bthread/mutex.h"
+#include "butil/macros.h"
+#include "common/status.h"
+#include "util/wait_group.h"
+
+namespace arcanedb {
+namespace util {
+
+template <typename T, typename Key = std::string_view> class SingleFlight {
+public:
+  SingleFlight() = default;
+  ~SingleFlight() = default;
+
+  using LoadFunc =
+      std::function<Status(const Key &key, std::shared_ptr<T> *entry)>;
+
+  /**
+   * @brief
+   * SingleFlight only guarantte that loader won't get called concurrently.
+   * It won't guarantee that loader will only get called once.
+   * So user should make sure that loader has some double check logic to achieve
+   * "exactly once" semantic.
+   * @param key
+   * @param entry
+   * @param loader
+   * @return Status
+   */
+  Status Do(const Key &key, std::shared_ptr<T> *entry,
+            LoadFunc loader) noexcept {
+    mu_.lock();
+    auto it = map_.find(key);
+    if (it != map_.end()) {
+      auto call = it->second;
+      mu_.unlock();
+      call->wg.Wait();
+
+      *entry = call->entry;
+      return call->st;
+    }
+
+    // initialize control struct
+    auto call = std::make_shared<Call>();
+    map_.emplace(key, call);
+    mu_.unlock();
+
+    // call loader
+    call->st = loader(key, &call->entry);
+    call->wg.Done();
+
+    // remove it from map
+    mu_.lock();
+    map_.erase(key);
+    mu_.unlock();
+
+    *entry = call->entry;
+    return call->st;
+  }
+
+private:
+  DISALLOW_COPY_AND_ASSIGN(SingleFlight);
+
+  struct Call {
+    WaitGroup wg{1};
+    std::shared_ptr<T> entry;
+    Status st;
+  };
+
+  bthread::Mutex mu_;
+  absl::flat_hash_map<Key, std::shared_ptr<Call>> map_; // guarded by mu_
+};
+
+} // namespace util
+} // namespace arcanedb
---
+++ test/util/singleflight_test.cpp
@@ -0,0 +1,51 @@
+/**
+ * @file singleflight_test.cpp
+ * @author sheep (ysj1173886760@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-01-02
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "util/bthread_util.h"
+#include "util/singleflight.h"
+#include "util/wait_group.h"
+#include <atomic>
+#include <gtest/gtest.h>
+#include <string>
+
+namespace arcanedb {
+namespace util {
+
+TEST(SingleFlightTest, BasicTest) {
+  auto single_flight = SingleFlight<int>();
+  bthread::Mutex mu;
+  int counter = 0;
+  std::unordered_set<std::string> flag;
+  auto loader = [&](const std::string_view &key, std::shared_ptr<int> *entry) {
+    std::lock_guard<bthread::Mutex> guard(mu);
+    if (!flag.count(std::string(key))) {
+      flag.emplace(std::string(key));
+      counter += 1;
+    }
+    return Status::Ok();
+  };
+  WaitGroup wg(10 * 100);
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 100; j++) {
+      LaunchAsync([&, index = i]() {
+        std::shared_ptr<int> entry;
+        auto s = single_flight.Do(std::to_string(index), &entry, loader);
+        EXPECT_EQ(s, Status::Ok());
+        wg.Done();
+      });
+    }
+  }
+  wg.Wait();
+  EXPECT_EQ(counter, 10);
+}
+
+} // namespace util
+} // namespace arcanedb
