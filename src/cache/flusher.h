90c5f8ebe7ae436d745698cbbec0d29a6c2fa6cd
ysj1173886760 <1173886760@qq.com>
2023-03-12 21:00:05 +0800

    add some member variable for flusher

--- src/cache/flusher.h
+++ src/cache/flusher.h
@@ -1,14 +1,28 @@
 #pragma once
 
+#include "bthread/condition_variable.h"
 #include "bthread/mutex.h"
+#include "cache/buffer_pool.h"
+#include "util/wait_group.h"
 
 namespace arcanedb {
 namespace cache {
 
 class Flusher {
 public:
+  void Start() noexcept;
+
+  void Stop() noexcept;
+
+  void InsertDirtyPage(const BufferPool::PageHolder &page) noexcept;
+
 private:
+  std::deque<BufferPool::PageHolder> deque_;
+  bthread::ConditionVariable cv_;
   bthread::Mutex mu_;
+
+  util::WaitGroup wg_;
+  std::atomic_bool stop_{false};
 };
 
 } // namespace cache
